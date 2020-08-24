fe9d2666c75d995dd1e8adcb49f868f317c090ae
Efe <61231517+aefecoban@users.noreply.github.com>
2020-08-24 19:40:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 
     $file = "route.php";
 
+    $dirName = dirname($_SERVER["SCRIPT_NAME"]);
+    $baseName = basename($_SERVER["SCRIPT_NAME"]);
+
+    $url = str_replace([$dirName, $baseName], "", $_SERVER["REQUEST_URI"]);
+
     if(file_exists($file)){
         require_once($file);
     }else{
         die();
     }
+
+    $route = new Route;
+
+    $route->add("","home.php");
+    $route->add("/","home.php");
+    $route->add("/anasayfa","home.php");
+
+    $route->show($url);
 
 ?>
